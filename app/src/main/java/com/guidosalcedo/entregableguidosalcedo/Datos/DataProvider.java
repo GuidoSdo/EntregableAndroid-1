cbf7a9c6965f98c9de080c2c9ed0308969f15aad
Unknown <guido.salcedo96@gmail.com>
2018-06-08 21:55:45 -0300

    Entregable terminado final
    Falta agregar imagenes y algunos txt

--- app/src/main/java/com/guidosalcedo/entregableguidosalcedo/Datos/DataProvider.java
+++ app/src/main/java/com/guidosalcedo/entregableguidosalcedo/Datos/DataProvider.java
@@ -13,10 +13,16 @@ public class DataProvider {
 
     public static List<Receta> listaDeRecetas = new ArrayList<>();
 
+
     public  List<Receta> getListaRecetas() {
 
 
-        listaDeRecetas.add(new Receta("Empanadas al estilo Cobarde", R.drawable.cobarde, "perro,azucar,agua,ajo", "cocinar a fuego lento"));
+        listaDeRecetas.add(new Receta("Empanadas de Carne", R.drawable.empanada, "24 tapas de empanada, 1/2 kg carne picada, 2 cebollas, 2 huevos +" +
+                "sal, pimienta, oregano a gusto", "1- Cortar cebollas y colocar en recipiente" +
+                "2- Agregar carne picada y condimentar" +
+                "3- Dejar asentar la mezcla" +
+                "4- Rellenar tapas con relleno" +
+                "5- Cocinar en horno a temperatura 180°c"));
         listaDeRecetas.add(new Receta("Sopa de Dexter", R.drawable.dexter, "carne humana,azucar,agua, especias tropicales, manzanas", "cocinar a fuego semirapido"));
         listaDeRecetas.add(new Receta("Canelones al estilo Goku", R.drawable.goku, "carne de Saiyajin,azucar,agua, 4 esferas del dragon", "cocinar  a fuego rapido"));
         listaDeRecetas.add(new Receta("Ensalada rusa Heman", R.drawable.heman, "carne de heman,azucar,agua,leche, lechuga", "cocinar a fuego lento"));
